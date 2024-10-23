b45835dbe620dcd33be9784d485d25851b1401a7
Gabriel Duarte <bieldusantos07@gmail.com>
2024-10-22 22:48:18 -0300

    Adding save model and new images

--- documentation/src/PC-Parts_Classificator.py
+++ documentation/src/PC-Parts_Classificator.py
@@ -7,7 +7,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 
-# Caminho relativo à pasta do script atual
+# Path to the current script folder
 current_dir = os.path.dirname(__file__)
 dataset_dir = os.path.join(current_dir, 'dataset')
 
@@ -18,7 +18,20 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     label_mode='int',
     batch_size=32,
     image_size=(256,256),
-    class_names=['cpu', 'gpu'],
+    validation_split=0.2, # 20% for validation
+    subset='training', # training subset
+    seed=123 # ensure that separation is consistent
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    directory=dataset_dir, # root directory of the images
+    labels ='inferred',
+    label_mode='int',
+    batch_size=32,
+    image_size=(256,256),
+    validation_split=0.2, # 20% for validation
+    subset='validation', # validation subset
+    seed=123 # ensure that separation is consistent
 )
 
 # Basic CNN
@@ -30,19 +43,29 @@ model = Sequential([
     Flatten(),
     Dense(128, activation='relu'),
     Dense(64, activation='relu'),
-    Dense(2, activation='softmax'), # 2 classes (CPU and GPU)
+    Dense(14, activation='softmax'), # 2 classes (CPU and GPU)
 ])
 
 # Compiling the model
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Training
-history = model.fit(train_ds, epochs = 10, batch_size = 32)
+history = model.fit(train_ds, # training data
+                    validation_data=val_ds,
+                    epochs = 20,
+                    verbose=1)
+
+# Saving the model
+model.save('pc_parts_classification_model.h5')
+
+# Evaluating the model on validation data
+val_loss, val_acc = model.evaluate(val_ds)
+print(f'Acc in validation set: {val_acc:.2f}')
 
 # Visualize accuracy and loss during training
-plt.plot(history.history['accuracy'], label='Accuracy')
-plt.plot(history.history['loss'], label='Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Metrics')
-plt.legend()
-plt.show()
+#plt.plot(history.history['accuracy'], label='Accuracy')
+#plt.plot(history.history['loss'], label='Loss')
+#plt.xlabel('Epochs')
+#plt.ylabel('Metrics')
+#plt.legend()
+#plt.show()

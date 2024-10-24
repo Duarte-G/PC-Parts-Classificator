992aff087c431a73b4e269acdb5d5f711507edd3
Gabriel Duarte <bieldusantos07@gmail.com>
2024-10-24 20:36:38 -0300

    Improved model and adding images

--- documentation/src/PC-Parts_Classificator.py
+++ documentation/src/PC-Parts_Classificator.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, RandomFlip, RandomRotation, RandomZoom, GlobalAveragePooling2D
-from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.callbacks import EarlyStopping
 
@@ -14,13 +13,19 @@ from tensorflow.keras.callbacks import EarlyStopping
 current_dir = os.path.dirname(__file__)
 dataset_dir = os.path.join(current_dir, 'dataset')
 
-# Dataset for CPU and GPU
+# Data augmentation layers
+data_augmentation = Sequential([
+    RandomFlip("horizontal"),
+    RandomRotation(0.1),
+    RandomZoom(0.1),
+])
+
 train_ds = tf.keras.utils.image_dataset_from_directory(
     directory=dataset_dir, # root directory of the images
     labels ='inferred',
     label_mode='int',
     batch_size=32,
-    image_size=(256,256),
+    image_size=(224, 224), # changing image size to 224x224
     validation_split=0.2, # 20% for validation
     subset='training', # training subset
     seed=123 # ensure that separation is consistent
@@ -31,39 +36,42 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     labels ='inferred',
     label_mode='int',
     batch_size=32,
-    image_size=(256,256),
+    image_size=(224, 224), # changing image size to 224x224
     validation_split=0.2, # 20% for validation
     subset='validation', # validation subset
     seed=123 # ensure that separation is consistent
 )
 
-#base_model = MobileNetV2(input_shape=(256, 256, 3), include_top=False, weights='imagenet')
-#base_model.trainable = False  # Congela os pesos da base
+# Cache, shuffle, and prefetch data to optimize performance
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-# Carregar o modelo VGG16 pré-treinado, sem a camada final (include_top=False)
-base_model = VGG16(input_shape=(256, 256, 3), include_top=False, weights='imagenet')
-# Congela os pesos das camadas da base VGG16 para não serem treinadas novamente
+# Load pre-trained VGG16 without top
+base_model = VGG16(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+# Freeze the VGG16 base layers so they can't be trained
 base_model.trainable = False
 
 early_stopping = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
 
-
-# Basic CNN
+# Pre-trained model
 model = Sequential([
-    base_model,
-    GlobalAveragePooling2D(),
-    Dense(128, activation='relu'),
+    RandomFlip("horizontal"),
+    RandomRotation(0.1),
+    RandomZoom(0.1),
+    base_model,  # VGG16 without top
+    GlobalAveragePooling2D(),  # Pooling global para reduzir as dimensões
+    Dense(128, activation='relu'),  # Additional dense layer
     Dense(64, activation='relu'),
-    Dense(10, activation='softmax'), # 10 classes (folders)
+    Dense(10, activation='softmax')  # 10 classes
 ])
-
 # Compiling the model
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Training
 history = model.fit(train_ds, # training data
                     validation_data=val_ds,
-                    epochs = 20,
+                    epochs = 40,
                     verbose=1,
                     callbacks=[early_stopping])
 
@@ -75,9 +83,9 @@ val_loss, val_acc = model.evaluate(val_ds)
 print(f'Acc in validation set: {val_acc:.2f}')
 
 # Visualize accuracy and loss during training
-#plt.plot(history.history['accuracy'], label='Accuracy')
-#plt.plot(history.history['loss'], label='Loss')
-#plt.xlabel('Epochs')
-#plt.ylabel('Metrics')
-#plt.legend()
-#plt.show()
+plt.plot(history.history['accuracy'], label='Accuracy')
+plt.plot(history.history['loss'], label='Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Metrics')
+plt.legend()
+plt.show()
